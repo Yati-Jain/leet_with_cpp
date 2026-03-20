feb0098b3e6de27dfafa2f42386c7638e3dcb136
Yati-jain <yatijain25@gmail.com>
2026-03-20 23:49:30 +0530

    isPrime

---
+++ maths/prime.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+string isprime(int n){
+    for (int i=2; i*i <= n; i++ ){
+        if(n%i==0){
+            return "non prime";
+        }
+    }
+    return "prime";
+}
+
+int main (){
+    int n = 42;
+    cout<< isprime(n)<< endl;
+    return 0;
+}
